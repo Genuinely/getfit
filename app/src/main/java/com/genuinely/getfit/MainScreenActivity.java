a4137087412b60aebc3f815d7b4af1090d016a68
Neil Yeung <neil.y.yeung@gmail.com>
2019-01-05 00:26:30 -0800

    Added portrait and landscape fragments
    (1) Added portrait and landscape fragments.
    (2) Added fragment configuration management in MainScreenActivity

--- app/src/main/java/com/genuinely/getfit/MainScreenActivity.java
+++ app/src/main/java/com/genuinely/getfit/MainScreenActivity.java
@@ -1,5 +1,8 @@
 package com.genuinely.getfit;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,20 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Configuration config = getResources().getConfiguration();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            WorkoutLandscapeFragment workoutLandscapeFragment = new WorkoutLandscapeFragment();
+            fragmentTransaction.replace(android.R.id.content, workoutLandscapeFragment);
+
+        } else {
+            WorkoutPortraitFragment workoutPortraitFragment = new WorkoutPortraitFragment();
+            fragmentTransaction.replace(android.R.id.content, workoutPortraitFragment);
+        }
+        fragmentTransaction.commit();
     }
 }
